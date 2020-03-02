d7c666731ba7fb69f6815e2acef5b363c2f53201
hacktorco <hacktorco@gmail.com>
2020-03-02 22:19:32 +0330

    add user list widget

---
+++ app.client/src/gui/styles/pages/main_window_widgets/users_list_widgets_styles.py
@@ -0,0 +1,18 @@
+
+main_widget_styles = ("main_widget_styles", """
+    [accessibleName="main_widget_styles"] {
+        min-width: 170px;
+        max-width: 170px;
+        max-height: 600px;
+        min-height: 600px;
+    }
+""")
+
+scroll_layout_styles = ("scroll_layout_styles", """
+    [accessibleName="scroll_layout_styles"] {
+        min-width: 170px;
+        max-width: 170px;
+        max-height: 600px;
+        min-height: 600px;
+    }
+""")
--- app.client/src/gui/windows/window_handler/main_window_handler/mainwindow.py
+++ app.client/src/gui/windows/window_handler/main_window_handler/mainwindow.py
@@ -4,12 +4,12 @@
 """
 
 from PyQt5.QtWidgets import (
-    QMainWindow, QWidget, QGridLayout, QVBoxLayout,QGroupBox
+    QMainWindow, QWidget, QHBoxLayout, 
+    QGroupBox, QGridLayout
 )
 from PyQt5.QtCore import Qt
 
 from gui.styles.windows.main_window_style import *
-from .contact_list_widget import ContactList
 from interfaces.observer_pattern.observer import Observer
 from core.inner_concentrate.concentrate import ConcentrateSubject
 from gui.components.message_box import MessageBox
@@ -20,15 +20,13 @@ class MainWindow(Observer, QMainWindow):
         super(MainWindow, self).__init__(parent=parent)
         self.concentrate_subject = ConcentrateSubject()
         self.concentrate_subject.attach(self)
-
+        self.setLayoutDirection(Qt.RightToLeft)
         self.socket_server = socket_server
 
         self.setWindowTitle("main window")
-        self.setContentsMargins(0, 0, 0, 0)
-        self.setLayoutDirection(Qt.RightToLeft)
         self.setAccessibleName(main_window_styles[0])
         self.setStyleSheet(main_window_styles[1])
-
+                
         self.__set_main_widget__()
         self.__signal_to_socket__()
 
@@ -55,15 +53,14 @@ class MainWindow(Observer, QMainWindow):
         """ Add centeral widget for switching between widgets in main window
             :return:
         """
-        main_widget = QWidget(self)
-        main_widget.setContentsMargins(0, 0, 0, 0)
-        # add widget to main widget
-        self.main_widget_layout = QGridLayout(main_widget)
-        self.main_widget_layout.setContentsMargins(0, 0, 0, 0)
-        contact_list_widget = ContactList()
-        self.main_widget_layout.addWidget(contact_list_widget)
 
-        self.setCentralWidget(main_widget)
+        self.main_widget_layout = QHBoxLayout()
+        self.main_widget_layout.setStretch(0, 0)
+        self.main_widget_layout.setContentsMargins(0, 0, 0, 0)
+
+        from ...pages.main_window_widgets.users_list_widgets import UserListWidget
+        user_widget_main = UserListWidget(parent=self)
+        self.main_widget_layout.addWidget(user_widget_main)
 
     def execute_app(self):
         self.show()
